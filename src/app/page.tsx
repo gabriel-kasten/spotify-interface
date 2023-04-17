892eb82ca5da6b541351fab7b2ebde510fe503ed
Gabriel Kasten <gabrieldegkasten@gmail.com>
2023-04-17 17:00:51 -0300

    component footer and sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Footer from "@/components/Footer";
+import { Sidebar } from "@/components/Sidebar";
 import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList, Laptop2, Volume, Maximize2 } from "lucide-react";
 import Image from "next/image";
 
@@ -5,33 +7,9 @@ export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
-        <aside className="w-72 bg-zinc-950 p-6">
-          <nav className="space-y-5">
-            <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
-              <HomeIcon />
-              Home
-            </a>
-            <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
-              <Search />
-              Search
-            </a>
-            <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
-              <Library />
-              Your Library
-            </a>
-          </nav>
 
-          <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3">
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">METAL GUEAR RISING: REVENGEANCE</a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">You Give Love A Bad Name</a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Pop Internacional</a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">80's Japonese City Pop</a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Pfvr Não Eu Tenho Família</a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Minha playlist #9</a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Vibe Manhã 😴🥱</a>
-          </nav>
+        <Sidebar />
 
-        </aside>
         <main className="flex-1 p-6">
           <div className="flex items-center gap-4">
             <button className="rounded-full bg-black/40 p-1">
@@ -121,45 +99,7 @@ export default function Home() {
         </main>
       </div>
 
-      <footer className="bg-zinc-900 border-t border-zinc-700 px-6 py-4 flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <Image src="/album_cover.jpg" width={56} height={56} alt="Album Cover" />
-          <div className="flex flex-col">
-            <strong className="text-normal">Rope</strong>
-            <span className="text-xs text-zinc-400">Foo Fighters</span>
-          </div>
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <div className="flex items-center gap-6">
-            <Shuffle size={20} className="text-zinc-200" />
-            <SkipBack size={20} className="text-zinc-200" />
-            <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black">
-              <Play />
-            </button>
-            <SkipForward size={20} className="text-zinc-200" />
-            <Repeat size={20} className="text-zinc-200" />
-          </div>
-          <div className="flex items-center gap-2">
-            <span className="text-xs text-zinc-400">0:31</span>
-            <div className="h-1 rounded-full w-96 bg-zinc-600">
-              <div className="bg-zinc-200 w-40 h-1 rounded-full"></div>
-            </div>
-            <span className="text-xs text-zinc-400">2:14</span>
-          </div>
-        </div>
-        <div className="flex items-center gap-2">
-          <Mic2 size={20} />
-          <LayoutList size={20} />
-          <Laptop2 size={20} />
-          <div className="flex items-center gap-2">
-            <Volume size={20} />
-            <div className="h-1 rounded-full w-24 bg-zinc-600">
-              <div className="bg-zinc-200 w-10 h-1 rounded-full"></div>
-            </div>
-          </div>
-          <Maximize2 size={20} />
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,33 @@
+import { HomeIcon, Library, Search } from "lucide-react";
+
+export function Sidebar() {
+  return (
+    <aside className="w-72 bg-zinc-950 p-6">
+      <nav className="space-y-5">
+        <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
+          <HomeIcon />
+          Home
+        </a>
+        <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
+          <Search />
+          Search
+        </a>
+        <a href="" className="flex items-center gap-3 text-sm font-semibold text-zinc-200">
+          <Library />
+          Your Library
+        </a>
+      </nav>
+
+      <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3">
+        <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">METAL GUEAR RISING: REVENGEANCE</a>
+        <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">You Give Love A Bad Name</a>
+        <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Pop Internacional</a>
+        <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">80's Japonese City Pop</a>
+        <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Pfvr Não Eu Tenho Família</a>
+        <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Minha playlist #9</a>
+        <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Vibe Manhã 😴🥱</a>
+      </nav>
+
+    </aside>
+  )
+}
